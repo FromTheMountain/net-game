e542a5a8425a09193dd93ba2c7d6607efef23d17
Jeroen <jeroenvdb@live.nl>
2020-08-29 22:43:19 +0200

    changed to svg

--- app.js
+++ app.js
@@ -10,26 +10,11 @@ function initializeBoard() {
 
     for (let i = 0; i < HEIGHT; i++) {
         for (let j = 0; j < WIDTH; j++) {
-            const tile = document.createElement('div');
-            tile.classList.add('net-tile');
-            tile.setAttribute('data-index', i * WIDTH + j);
-            tile.style = `left: ${50 * j}px; top: ${50 * i}px`;
-
-            const tileCanvas = document.createElement('canvas');
-            const ctx = tileCanvas.getContext('2d');
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(20, 20);
-            ctx.stroke();
-
-            tile.appendChild(tileCanvas);
-            game.appendChild(tileCanvas);
-
-
-            // game.innerHTML += `
-            //     <div class="net-tile" data-index="${i * WIDTH + j}" style="left: ${50 * j}px; top: ${50 * i}px">
-            //     </div>
-            // `
+            game.innerHTML += `
+                <svg class="net-tile" data-index=${i * WIDTH + j} height="50" width="50" style="left: ${50 * j}px; top: ${50 * i}px">
+                    <line x1="0" y1="0" x2="20" y2="20"/>
+                </svg>
+            `
         }
     }
 }
@@ -39,11 +24,9 @@ function makeConnections() {
 }
 
 game.addEventListener('click', e => {
-    if (e.target.parentNode.classList.contains('net-tile')) {
+    if (e.target.classList.contains('net-tile')) {
         console.log('you clicked a tile');
         const tileIndex = e.target.getAttribute('data-index');
-
-        console.log(tileIndex);
 
         rotations[tileIndex] += 90;
         e.target.style.transform = `rotate(${rotations[tileIndex]}deg)`;
